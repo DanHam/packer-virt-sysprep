bb341325768522abb097be07662194c13d5f3e17
DanHam <DanHam@users.noreply.github.com>
2016-06-22 12:30:29 +0100

    Add script to delete dhcp client lease data

---
+++ sysprep-op-dhcp-client-state.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Remove DHCP client lease information
+
+LEASE_DATA_LOCATIONS=(
+    "/var/lib/dhclient/*"
+    "/var/lib/dhcp/*"
+)
+
+# Include hidden files in glob
+shopt -s nullglob dotglob
+
+for LEASE_DATA in ${LEASE_DATA_LOCATIONS[@]}
+do
+    rm -rf ${LEASE_DATA}
+done
