5a22a218f7bbf9eef49a3d666cc7711be367a55d
DanHam <DanHam@users.noreply.github.com>
2019-01-18 15:03:20 +0000

    Add script to remove all cloud-init run-time data and reinitialise

---
+++ sysprep-op-cloud-init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Remove all cloud-init run-time data and logs
+#
+# The removal completely resets cloud-init. When the instance is next
+# started cloud-init will run all configured modules as if running for the
+# first time
+rm -rf /var/lib/cloud/*
+rm -f /var/log/cloud-init.log
+
+exit 0
