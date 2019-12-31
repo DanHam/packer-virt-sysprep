afb4e929f338e06e1909dc7612b8b7a4b952250b
DanHam <DanHam@users.noreply.github.com>
2019-12-31 09:24:00 +0000

    Refactor to allow rm of lease file when it does not contain lease info
    Some implementations write out the DUID (DHCP Unique Identifier) to the
    dhcp leases file even when the interface has a static address assigned.
    This means the lease file is created but contains no actual lease info.
    
    The refactor ensures the lease file is always removed whatever the
    interface configuration or lease file contents

--- sysprep-op-dhcp-client-state.sh
+++ sysprep-op-dhcp-client-state.sh
@@ -13,32 +13,44 @@ lease_data_locations=(
 # Include hidden files in glob
 shopt -s nullglob dotglob
 
-for lease_file in ${lease_data_locations[@]}
-do
-    # On shutdown ifdown/dhclient may write to (or recreate) the dhcp
-    # leases file when the interface is brought down. To ensure the leases
-    # file is removed we need to wait for the interface to be brought down.
-    # Timeout after 20secs.
-    iface="$(cat ${lease_file} | sed -nre 's/.*interface "(.*)";/\1/p' | uniq)"
+# Ensure all interfaces are down prior to removing the leases file.
+# Otherwise the file is recreated by ifdown/dhclient
+for iface in $(ls /sys/class/net/ | grep -v lo); do
+    # Wait for each interface to be taken down. Timeout after 20 secs
     timer=0
     while grep up "/sys/class/net/${iface}/operstate" &>/dev/null && \
         [[ timer -lt 20 ]]; do
         sleep 1
         let timer=${timer}+1
     done
-    # If the interface was brought down successfully, wait a few secs
-    # for ifdown/dhclient to complete
-    sleep 2
-    # If we timed out we need to kill the dhcp client to prevent it
-    # recreating the dhcp leases file. Killing the client won't hurt if the
-    # interface was brought down sucessfully as the system is going down
-    # anyway
+
+    # If the interface is still up it is likely something has gone wrong
+    # with the usual procedures that take the interface down at shutdown.
+    # Make a best effort attempt to take the interface down manually
+    # temporarily ignoring errors
+    if grep up "/sys/class/net/${iface}/operstate" &>/dev/null; then
+        set +o errexit
+        ifdown ${iface}
+        set -o errexit
+    fi
+
+    # Some implementations start the dhcp client when the interface is taken
+    # down (even if the interface is statically configured). It is the dhcp
+    # client that writes out to the leases file when the interface goes
+    # down. Kill the client as a precautionary measure to prevent further
+    # interference. Ignore errors in case the dhclient exits between
+    # obtaining its pid and killing it
     pid="$(ps aux | grep /sbin/dhclient | grep "${iface}" | tr -s " " | \
         cut -d' ' -f2)"
     if [ "x${pid}" != "x" ]; then
+        set +o errexit
         kill -9 "${pid}"
+        set -o errexit
     fi
-    # Finally remove the leases file
+done
+
+# Now that all interfaces are down remove all lease files
+for lease_file in ${lease_data_locations[@]}; do
     rm -f ${lease_file}
 done
 
