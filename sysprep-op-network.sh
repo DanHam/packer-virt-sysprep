95d88cfbedbd702391d95d6daeb2d83a37be77b5
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-06 06:51:42 +0200

    Add netplan cleanup

--- sysprep-op-network.sh
+++ sysprep-op-network.sh
@@ -24,4 +24,15 @@ for network_config in "${network_config_locations[@]}"; do
     sed -i -e "/^HWADDR=/d" -e "/^UUID=/d" "${network_config}"
 done
 
+# This needs to be executed on the Ubuntu server installation (mini.iso) when installing Desktop environment using
+# `tasksel tasksel/first multiselect ubuntu-desktop`
+
+# By default Ubuntu server installation (mini.iso) creates the `/etc/netplan/01-netcfg.yaml` and `/etc/netplan/01-network-manager-all.yaml` which causes problems to Vagrant.
+# Some details can be found here: https://github.com/hashicorp/vagrant/issues/11378
+# In short the /etc/netplan/01-netcfg.yaml should not be on the Ubuntu Desktop installation when using Vagrant otherwise `vagrant up` is hanging.
+
+if [[ -s /etc/netplan/01-netcfg.yaml ]] ; then
+  rm /etc/netplan/01-netcfg.yaml
+fi
+
 exit 0
