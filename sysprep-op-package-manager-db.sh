66100003431b411022edd62e5cb7660bbc7696ad
DanHam <DanHam@users.noreply.github.com>
2021-05-20 18:28:12 +0100

    Remove APT lists

--- sysprep-op-package-manager-db.sh
+++ sysprep-op-package-manager-db.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 #
-# Remove any host specific RPM database files
+# Remove dynamically created package manager files
 #
-# RPM will recreate these files automatically if needed
 set -o errexit
 
+# RPM Host DB files. RPM will recreate these files automatically if needed
 rm -f /var/lib/rpm/__db.*
+
+# APT lists. APT will recreate these on the first 'apt update'
+apt_lists=/var/lib/apt/lists
+if [ -d "${apt_lists}" ]; then
+    find "${apt_lists}" -type f | xargs rm -f
+fi
 
 exit 0
